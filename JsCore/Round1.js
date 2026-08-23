5189535c64da290d10e473d0bdb10871d54bb350
Xzardd <farhanwork08@gmail.com>
2026-08-23 23:37:28 +0530

    Learnt New Concepts

--- JsCore/Round1.js
+++ JsCore/Round1.js
@@ -1,11 +1,37 @@
 let name = "Wiz";
 let age = 23;
+let city = "Chandigarh";
 
-function greet(name) {
-    return "Hello, " + name + "!";
+console.log("Hello, my name is " + name);
+console.log("I am " + age + " years old");
+console.log("I live in " + city);
+
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+let difference = b - a;
+let product = a * b;
+let division = b / a;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Division: " + division);
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
 }
 
-console.log(greet(name));
-console.log("Age:", age);
+for (let i = 1; i <= 5; i++) {
+    console.log("Number: " + i);
+}
 
-//inital learning
+function greet(name) {
+    console.log("Welcome, " + name);
+}
+
+greet("Wiz");
+greet("Alex");
