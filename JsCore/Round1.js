1401a4a1b2296aea72f950201441bf2753fe12e4
Xzardd <farhanwork08@gmail.com>
2026-09-26 02:25:32 +0530

    Learnt to Print

--- JsCore/Round1.js
+++ JsCore/Round1.js
@@ -1,1 +1,9 @@
 console.log("hello")
+
+const accountId = 23
+let accountName = "Raj"
+
+accountCity = "Patna"
+accountState = "Rest"
+
+console.table([accountId, accountName, accountCity, accountCity])
